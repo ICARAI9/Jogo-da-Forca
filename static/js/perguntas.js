52443b65661e7339d4815f9149ebf4d7572bbfa4
ICAROAUGUSTO <icaro25flash11@gmail.com>
2024-09-24 14:20:06 -0300

    24/09/2024
    Este código é um jogo da forca, sobre o livro Dexter, feito com HTML, CSS e JS.

---
+++ static/js/perguntas.js
@@ -0,0 +1,27 @@
+const palavras = [
+    "Vigilante",
+    "Código",
+    "Dualidade",
+    "Intimidade",
+    "Apoio",
+    "Contrastante",
+    "Surpresas",
+    "Ironia",
+   "Moralidade",
+    "Dinâmico",
+];
+  
+
+
+const dicas =[
+    "Qual é a premissa central da série Dexter",
+    "Como Dexter justifica suas ações de vigilante",
+    "Quais são os principais conflitos internos de Dexter",
+    "Como a perspectiva de Dexter influencia a narrativa",
+    "Quem são os principais personagens secundários e qual é o papel deles",
+    "De que maneira o ambiente de Miami contribui para a história",
+    "Quais são as principais reviravoltas no enredo",
+    "Como o humor negro afeta a percepção da história",
+    "Quais temas principais são explorados no livro",
+    "Como a estrutura e o ritmo do livro afetam o envolvimento do leitor",
+];
